a5288538ded214e03dbdc91e747cd8504eaf4b3e
hezhongxian <jhonhe7night@outlook.com>
2025-12-08 23:31:36 +0800

    add - init sql

---
+++ sql/init.sql
@@ -0,0 +1,19 @@
+INSERT INTO account_maps (id, keyword, account, `type`, created_at, updated_at) VALUES(1, '招商银行信用卡(2035)', 'Liabilities:CMBCreditCard:2035', 'asset', '2025-04-26 13:22:02', '2025-04-26 21:25:45.317');
+INSERT INTO account_maps (id, keyword, account, `type`, created_at, updated_at) VALUES(3, '花呗', 'Liabilities:Huabei', 'asset', '2025-04-26 13:22:02', '2025-04-26 13:22:09');
+INSERT INTO account_maps (id, keyword, account, `type`, created_at, updated_at) VALUES(4, '余额宝', 'Assets:AliPay:Balance', 'asset', '2025-04-26 13:22:02', '2025-04-26 13:22:09');
+INSERT INTO account_maps (id, keyword, account, `type`, created_at, updated_at) VALUES(5, 'Yu‘E Bao', 'Assets:AliPay:Balance', 'asset', '2025-04-26 13:22:02', '2025-04-26 13:22:09');
+INSERT INTO account_maps (id, keyword, account, `type`, created_at, updated_at) VALUES(6, '地铁', 'Expenses:Transport', 'expense', '2025-04-26 13:22:02', '2025-04-26 13:22:09');
+INSERT INTO account_maps (id, keyword, account, `type`, created_at, updated_at) VALUES(7, '中运交通', 'Expenses:Transport', 'expense', '2025-04-26 13:22:02', '2025-04-26 13:22:09');
+INSERT INTO account_maps (id, keyword, account, `type`, created_at, updated_at) VALUES(8, '基金管理', 'Income:Fund', 'income', '2025-04-26 13:22:02', '2025-04-26 13:22:09');
+INSERT INTO account_maps (id, keyword, account, `type`, created_at, updated_at) VALUES(9, '光大银行', 'Assets:CEB:1027', 'asset', '2025-04-26 13:22:02', '2025-04-26 13:22:09');
+INSERT INTO account_maps (id, keyword, account, `type`, created_at, updated_at) VALUES(10, '招商银行(3229)', 'Assets:CMB:3229', 'asset', '2025-04-26 13:22:02', '2025-04-26 13:22:09');
+INSERT INTO account_maps (id, keyword, account, `type`, created_at, updated_at) VALUES(11, '零钱', 'Assets:WeChat', 'asset', '2025-04-26 13:22:02', '2025-04-26 13:22:09');
+INSERT INTO account_maps (id, keyword, account, `type`, created_at, updated_at) VALUES(12, '招商银行储蓄卡(3229)', 'Assets:CMB:3229', 'asset', '2025-04-26 13:22:02', '2025-04-26 13:22:09');
+INSERT INTO account_maps (id, keyword, account, `type`, created_at, updated_at) VALUES(13, '转账备注', 'Income:RedEnvelope', 'income', '2025-04-26 13:22:02', '2025-04-26 13:22:09');
+INSERT INTO account_maps (id, keyword, account, `type`, created_at, updated_at) VALUES(14, '广发银行储蓄卡(1508)', 'Assets:CGB:41508', 'asset', '2025-04-26 13:22:02', '2025-04-26 13:22:09');
+
+
+
+INSERT INTO beango_configs (id, created_at, updated_at, config_key, config_value, note) VALUES(1, '2025-05-17 13:17:02.133', '2025-05-17 13:17:02.133', 'defaultFolder', '0-default', NULL);
+INSERT INTO beango_configs (id, created_at, updated_at, config_key, config_value, note) VALUES(2, '2025-05-17 13:40:59.076', '2025-05-17 13:40:59.076', 'securitFolder', '1-securities', NULL);
+INSERT INTO beango_configs (id, created_at, updated_at, config_key, config_value, note) VALUES(3, '2025-05-17 13:41:32.201', '2025-05-17 13:41:32.201', 'outputFolder', './output', NULL);
