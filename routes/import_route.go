134a8f69ef6537ee5dd15bba405a00483bbc6b37
hezhongxian <jhonhe7night@outlook.com>
2025-12-17 11:13:26 +0800

    delete - file tree & test html

--- routes/import_route.go
+++ routes/import_route.go
@@ -9,7 +9,4 @@ import (
 func RegisteImportRoutes(r *gin.Engine) {
 	r.POST("/upload/alipay_csv", service.ImportAlipayCSV)
 	r.POST("/upload/wechat_csv", service.ImportWechatCSV)
-	// 新增
-	r.GET("/api/files/tree", service.GetFileTree)
-	r.GET("/api/files/content", service.GetFileContent)
 }
